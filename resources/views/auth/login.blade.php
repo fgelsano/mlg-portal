11f4ba22358bd47c6035c1b1517fcdfd065b896a
Francis Gelsano <fgelsano@gmail.com>
2020-05-24 14:11:56 +0800

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,85 @@
+@extends('layouts.admin')
+
+@section('title','Login')
+
+@section('styles')
+
+@endsection
+
+@section('content')
+<div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+    <img src="{{ asset('admin/img/logo.png') }}" alt="MLGCL Logo" class="img-responsive my-5 d-md-block d-none">
+    <img src="{{ asset('admin/img/logo.jpg') }}" alt="MLGCL Logo" class="img-responsive mb-3 mt-5 d-block d-md-none" width="50%">
+
+      <div class="col-xl-10 col-lg-12 col-md-9">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <div class="col-lg-6 d-none d-lg-block bg-login-image" style="background-image: url({{asset('admin/img/cover-building.jpg')}})"></div>
+              <div class="col-12 col-lg-6">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                  </div>
+                  <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="form-group">
+                        <input id="email" placeholder="Enter Email Address..." type="email" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="form-group">                            
+                        <input id="password" type="password" placeholder="Password" class="form-control form-control-user @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                      <div class="custom-control custom-checkbox small">
+                        <input class="custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                        <label class="custom-control-label" for="customCheck">Remember Me</label>
+                      </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                        {{ __('Login') }}
+                    </button>
+                  <hr>
+                  <div class="text-center">
+                    @if (Route::has('password.request'))
+                        <a class="btn btn-link small" href="{{ route('password.request') }}">
+                            {{ __('Forgot Your Password?') }}
+                        </a>
+                    @endif
+                  </div>
+                </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+</div>
+@endsection
+
+
+
+
---
+++ resources/views/front-portal.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+
+            .mb-0{
+                margin-bottom: 0%;
+            }
+
+            .mb-5{
+                margin-bottom: 3rem;
+            }
+
+            .mt-0{
+                margin-top: 0%;
+            }
+            
+            .links-btn{
+                padding: 10px 30px !important;
+            }
+
+            .links-btn:hover{
+                background-color: #6C7477;
+                color: #fff;
+
+                border-radius: 100px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+
+                <img src="{{ asset('storage/MLG_Logo-Since-1999.jpg') }}" alt="MLG Logo">
+
+                <div class="title m-b-md mb-0">
+                    This is a Restricted Area.
+                </div>
+                <h3 class="mt-0 mb-5">Please go back to the front page or login with your credentials.</h3>
+
+                @if (Route::has('login'))
+                    <div class="links">
+                        @auth
+                            <a href="{{ url('/dashboard') }}" class="links-btn">Home</a>
+                        @else
+                            <a href="{{ route('login') }}" class="links-btn">Login</a>
+                            <a href="https://mlgcl.edu.ph" class="links-btn">Front Page</a>
+                        @endauth
+                    </div>
+                @endif
+            </div>
+        </div>
+    </body>
+</html>
